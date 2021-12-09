0f7f5f9558cef948c5ee8f427f7340bb9d7e66fb
Akhila4144 <orugantiakhila34@gmail.com>
2021-12-09 18:37:27 +0530

    UC2 Done

---
+++ AddressBookUC-2/ContactPerson.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookUC_2
+{
+    class ContactPerson
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+        public double PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public void displayContact()
+        {
+            Console.WriteLine("FirstName:" + this.FirstName + "LastName:" + this.LastName + "Address:" + this.Address + "City:" + this.City + "State:" + this.State + "ZipCode:" + this.ZipCode + "PhoneNumber:" + this.PhoneNumber + "Email:" + this.Email);
+        }
+        public void AddContact()
+        {
+            Console.WriteLine("Enter FirstName,LastName,AddressBookSystem,City,State,ZipCode,PhoneNumber,Email\n");
+
+            ContactPerson objcontact = new ContactPerson();
+            {
+
+                FirstName = Console.ReadLine();
+                LastName = Console.ReadLine();
+                Address = Console.ReadLine();
+                City = Console.ReadLine();
+                State = Console.ReadLine();
+                ZipCode = Convert.ToInt32(Console.ReadLine());
+                PhoneNumber = Convert.ToDouble(Console.ReadLine());
+                Email = Console.ReadLine();
+
+            };
+            List<ContactPerson> objList = new List<ContactPerson>();
+            objList.Add(objcontact);
+            objcontact.displayContact();
+        }
+    }
+}
---
+++ AddressBookUC-2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AddressBookUC_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContactPerson contactPerson = new ContactPerson();
+            contactPerson.AddContact();
+        }
+    }
+}
